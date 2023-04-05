fe08c9a9a995591e59ced1d63ab79e11f2753727
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2023-04-05 14:31:23 -0700

    Fixes duplicate in SchemaViewer

--- specifyweb/frontend/js_src/lib/components/SchemaViewer/Relationships.tsx
+++ specifyweb/frontend/js_src/lib/components/SchemaViewer/Relationships.tsx
@@ -14,6 +14,7 @@ import { TableIcon } from '../Molecules/TableIcon';
 import { localizedRelationshipTypes } from '../SchemaConfig/helpers';
 import type { SchemaViewerRow, SchemaViewerValue } from './helpers';
 import { SchemaViewerTableList } from './TableList';
+import { commonColumns } from './Fields';
 
 export function SchemaViewerRelationships({
   table,
@@ -90,15 +91,7 @@ export function SchemaViewerRelationships({
 const relationshipColumns = f.store(
   () =>
     ({
-      name: getField(schema.models.SpLocaleContainerItem, 'name').label,
-      label: schemaText.fieldLabel(),
-      description: schemaText.description(),
-      isHidden: getField(schema.models.SpLocaleContainerItem, 'isHidden').label,
-      isReadOnly: schemaText.readOnly(),
-      isRequired: getField(schema.models.SpLocaleContainerItem, 'isRequired')
-        .label,
-      type: getField(schema.models.SpLocaleContainerItem, 'type').label,
-      databaseColumn: schemaText.databaseColumn(),
+      ...commonColumns,
       relatedModel: schemaText.relatedModel(),
       otherSideName: schemaText.otherSideName(),
       isDependent: schemaText.dependent(),
